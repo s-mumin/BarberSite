cb4f6c9aa24278b53b23e74c4e3664b08744ba44
s-mumin <93403897+s-mumin@users.noreply.github.com>
2025-03-16 22:47:47 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,9 +2,15 @@
 var menuBtn = document.querySelector("#navbar-menu-button")
 var menuIcon = document.querySelector(".fa-bars")
 var mobileMenu = document.querySelector('.navbar-links-mobile')
+var navbar = document.getElementById('navbar')
+
 
 menuBtn.addEventListener('click', () => {
+
+
     mobileMenu.classList.toggle('navbar-links-mobile-active')
-    // attempting to toggle id instead of class  
+
     menuIcon.classList.toggle('fa-x')
+
+    navbar.classList.toggle('dark')
 })
